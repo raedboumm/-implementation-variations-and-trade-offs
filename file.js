ad4849e7db7cc8a12b60daad8d4ae3252fdfe0db
raedboumm <raedboumm@gmail.com>
2026-06-12 21:27:57 +0100

    Add MinHeap priority queue implementation
    Implement a MinHeap priority queue with insert, extractMin, peekMin, and isEmpty methods.

---
+++ file.js
@@ -0,0 +1,59 @@
+class MinHeapPQ {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(element) {
+        this.heap.push(element);
+        this.bubbleUp(this.heap.length - 1);
+    }
+
+    extractMin() {
+        if (this.isEmpty()) return null;
+        if (this.heap.length === 1) return this.heap.pop();
+        
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown(0);
+        return min;
+    }
+
+    peekMin() {
+        return this.isEmpty() ? null : this.heap[0];
+    }
+
+    isEmpty() {
+        return this.heap.length === 0;
+    }
+
+    bubbleUp(index) {
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[parentIndex] <= this.heap[index]) break;
+            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+            index = parentIndex;
+        }
+    }
+
+    bubbleDown(index) {
+        while (true) {
+            let left = 2 * index + 1;
+            let right = 2 * index + 2;
+            let smallest = index;
+
+            if (left < this.heap.length && this.heap[left] < this.heap[smallest]) smallest = left;
+            if (right < this.heap.length && this.heap[right] < this.heap[smallest]) smallest = right;
+            
+            if (smallest === index) break;
+            [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+            index = smallest;
+        }
+    }
+}
+
+// Example Usage:
+const pq = new MinHeapPQ();
+pq.insert(10);
+pq.insert(5);
+pq.insert(20);
+console.log(pq.extractMin()); // 5
